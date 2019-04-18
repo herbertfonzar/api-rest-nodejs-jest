6ace8038fb56584eb544c5e2a5a8d88ea0965edc
herbert fonzar <herbertfonzar@gmail.com>
2019-04-18 14:51:25 -0300

    refactoring project

--- test/user.test.js
+++ test/user.test.js
@@ -2,11 +2,20 @@ const request = require('supertest');
 
 const app = require('../src/app');
 
-test('Deve listar todos os usuários', () => (
+test('Should list all users', () => (
   request(app).get('/users')
     .expect(200)
     .then((response) => {
       expect(response.body).toHaveLength(1);
-      expect(response.body[0]).toHaveProperty('name', 'John Doe');
+      expect(response.body[0]).toHaveProperty('name', 'Herbert Fonzar');
+    })
+));
+
+test('Should successfully insert user', () => (
+  request(app).post('/users')
+    .send({ name: 'Herbert Fonzar', mail: 'herbertfonzar@mail.com' })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('Herbert Fonzar');
     })
 ));
